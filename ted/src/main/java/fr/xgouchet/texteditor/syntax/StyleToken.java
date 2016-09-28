04ac459485d7560dc64afd58b37dd7f576414e3d
Aleksandr Simonenko <a.simonenko@innopolis.ru>
2016-09-28 16:27:37 +0300

    Done all work for preparing token classes for data storing.

---
+++ ted/src/main/java/fr/xgouchet/texteditor/syntax/StyleToken.java
@@ -0,0 +1,42 @@
+package fr.xgouchet.texteditor.syntax;
+
+public class StyleToken {
+    protected String styleName;
+    protected String color;
+    protected Boolean bold;
+    protected Boolean italic;
+
+    /**
+     *
+     * @param styleName Style Token name
+     * @param color Color in hex format without #
+     * @param bold Flag which shows that text must be bold
+     * @param italic Flag which shows that text must be italic
+     */
+
+    StyleToken(String styleName, String color, Boolean bold, Boolean italic) {
+        this.styleName = styleName;
+        this.color = color;
+        this.bold = bold;
+        this.italic = italic;
+    }
+
+    /**
+     * Operation have two paths one for comparing with Syntax Token
+     * for checking style names of Syntax Tokens.
+     * Other one for compering Style Tokens
+     * @param obj Object with which we compare current class
+     * @return Result of comparison operation
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SyntaxToken) {
+            return styleName.equals(((SyntaxToken) obj).styleName);
+        }
+        return obj instanceof StyleToken &&
+                styleName.equals(((StyleToken) obj).styleName) &&
+                color.equals(((StyleToken) obj).color) &&
+                bold.equals(((StyleToken) obj).bold) &&
+                italic.equals(((StyleToken) obj).italic);
+    }
+}
--- ted/src/main/java/fr/xgouchet/texteditor/syntax/SyntaxToken.java
+++ ted/src/main/java/fr/xgouchet/texteditor/syntax/SyntaxToken.java
@@ -3,12 +3,32 @@ package fr.xgouchet.texteditor.syntax;
 
 public class SyntaxToken {
     protected String word;
-    protected String typeName;
+    protected String styleName;
 
+    /**
+     *
+     * @param word Syntax item
+     * @param styleName Style name for this token
+     */
+    SyntaxToken(String word, String styleName) {
+        this.word = word;
+        this.styleName = styleName;
+    }
 
+    /**
+     * Operation have two paths one for comparing Syntax Tokens
+     * Other one for compering Style Tokens names
+     * @param obj Object with which we compare current class
+     * @return Result of comparison operation
+     *
+     */
     @Override
     public boolean equals(Object obj) {
-
-        return false;
+        if (obj instanceof StyleToken ) {
+            return styleName.equals(((StyleToken) obj).styleName);
+        }
+        return obj instanceof SyntaxToken &&
+                word.equals(((SyntaxToken) obj).word)&&
+                styleName.equals(((SyntaxToken) obj).styleName);
     }
 }
